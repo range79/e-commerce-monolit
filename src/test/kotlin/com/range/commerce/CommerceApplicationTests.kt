70ce6d8bb318a9e0583ef1f41d5130ea22059ce7
range79 <darkrange006@gmail.com>
2025-07-27 23:36:29 +0400

    init: project initalized

---
+++ src/test/kotlin/com/range/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.range.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
